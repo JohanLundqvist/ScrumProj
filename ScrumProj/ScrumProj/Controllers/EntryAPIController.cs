98de66485bc4a008cc36c348d883a3382f480395
Johan Lundqvist <johan_aik1@hotmail.com>
2019-01-30 14:01:53 +0100

    Entry, blogginlägg

---
+++ ScrumProj/ScrumProj/Controllers/EntryAPIController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScrumProj.Controllers
+{
+    public class EntryAPIController : Controller
+    {
+        // GET: EntryAPI
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
