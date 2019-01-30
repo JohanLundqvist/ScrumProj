ed91be831d8c70644c96915657b361fd400ec6af
DavidLingvall <36515541+DavidLingvall@users.noreply.github.com>
2019-01-30 14:49:24 +0100

    Adds a model for files.

---
+++ ScrumProj/ScrumProj/Models/File.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScrumProj.Models
+{
+    public class File
+    {
+        public int FileId { get; set; }
+        public int Name { get; set; }
+        public byte[] FileBytes { get; set; }
+    }
+}
