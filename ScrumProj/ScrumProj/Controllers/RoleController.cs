91c8e45471518049f5c356b855d3db7dc8ef7743
Joakim Asplund <joaash171@edunet.oru.se>
2019-02-01 13:56:38 +0100

    Profile 100% Role 50%

---
+++ ScrumProj/ScrumProj/Controllers/RoleController.cs
@@ -0,0 +1,101 @@
+﻿using ScrumProj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScrumProj.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext ctx = new ApplicationDbContext();
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            var roles = ctx.Roles.ToList();
+            return View(roles);
+
+        }
+
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Roles/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                ctx.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole()
+                {
+                    Name = collection["RoleName"]
+                });
+                ctx.SaveChanges();
+                ViewBag.ResultMessage = "Role created successfully !";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+        public ActionResult Delete(string RoleName)
+        {
+            var thisRole = ctx.Roles.Where(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            ctx.Roles.Remove(thisRole);
+            ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+        //
+        // GET: /Roles/Edit/5
+        public ActionResult Edit(string roleName)
+        {
+            var thisRole = ctx.Roles.Where(r => r.Name.Equals(roleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+
+            return View(thisRole);
+        }
+
+        //
+        // POST: /Roles/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Microsoft.AspNet.Identity.EntityFramework.IdentityRole role)
+        {
+            try
+            {
+                ctx.Entry(role).State = System.Data.Entity.EntityState.Modified;
+                ctx.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+        public ActionResult ManageUserRoles()
+        {
+            // prepopulat roles for the view dropdown
+            var list = ctx.Roles.OrderBy(r => r.Name).ToList().Select(rr =>
+            
+            new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+            return View();
+        }
+    }
+}
