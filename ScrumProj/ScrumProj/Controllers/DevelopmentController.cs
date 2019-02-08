70a7cd49d10a255052f9e11838372e0845db8f83
Tobias <tubbeh_97@hotmail.com>
2019-02-08 11:00:07 +0100

    d

---
+++ ScrumProj/ScrumProj/Controllers/DevelopmentController.cs
@@ -0,0 +1,23 @@
+﻿using ScrumProj.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScrumProj.Controllers
+{
+    public class DevelopmentController : Controller
+    {
+        // GET: Development
+        [Authorize]
+        public ActionResult DevelopmentWork(DevelopmentViewModel model)
+        {       if(ModelState.IsValid)
+            {
+
+            }
+
+                return View(model);
+        }
+    }
+}
