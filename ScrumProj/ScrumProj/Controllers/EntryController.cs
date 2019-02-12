f3fa3ad65ae60a7fb465635a34478e67a2699db9
Joakim Asplund <joaash171@edunet.oru.se>
2019-02-12 10:39:33 +0100

    Fixes

--- ScrumProj/ScrumProj/Controllers/EntryController.cs
+++ ScrumProj/ScrumProj/Controllers/EntryController.cs
@@ -12,6 +12,7 @@ namespace ScrumProj.Controllers
 {
     public class EntryController : Controller
     {
+        // Method
         [Authorize]
         public ActionResult PublishEntry(HttpPostedFileBase newFile, EntryViewModel model, string SelectBlogg, string searchInput, HttpPostedFileBase img)
         {
@@ -22,6 +23,7 @@ namespace ScrumProj.Controllers
             if (SelectBlogg == "1")
             IsFormal = true;
             Models.File ThisFile = new Models.File();
+
             // adds data to entry with file
             if (newFile != null)
             {
@@ -54,9 +56,9 @@ namespace ScrumProj.Controllers
                     Author = GetNameOfLoggedInUser(),
                     Formal = IsFormal,
                     ImageUrl = imageUrl
-                    });
-
+                });
             }
+            
             //adds data to file without file
             else
             {
@@ -82,40 +84,51 @@ namespace ScrumProj.Controllers
             }
             ctx.SaveChanges();
             
-                int postId = 100000000;
-                // gets the entry just saved to database
-                foreach (var f in ctx.Entries)
-                {
-                    postId = f.Id;
-                }
+            int postId = 100000000;
+
+            // gets the entry just saved to database
+            foreach (var f in ctx.Entries)
+            {
+                postId = f.Id;
+            }
             AddCategoryToDatabase(searchInput, postId);
             ctx.SaveChanges();
 
             return RedirectToAction("BlogPage"); 
         }
 
+
+
+        // Method
         public ProfileModel GetCurrentUser(string Id)
         {
             var ctx = new AppDbContext();
             var UserId = User.Identity.GetUserId();
             var appUser = ctx.Profiles.SingleOrDefault(u => u.ID == Id);
+
             return appUser;
         }
 
+
+
+        // Method
         [Authorize]
         public ActionResult BlogPage(EntryViewModel model)
         {
             model.loggedInUser = GetCurrentUser(User.Identity.GetUserId());
             model.ListOfEntriesToLoopInBlogView = new List<EntryViewModel>();
             model.ListOfInformalEntriesToLoopInBlogView = new List<EntryViewModel>();
+
             AppDbContext db = new AppDbContext();
             var ListofEntries = db.Entries.ToList();
+
             foreach (var entry in ListofEntries)
             {
                 if (entry.Formal == true)
                 {
                     var thisFileId = entry.fileId;
                     var FileToFetch = db.Files.SingleOrDefault(i => i.FileId == thisFileId);
+
                     model.ListOfEntriesToLoopInBlogView.Add(new EntryViewModel
                     {
                         entry = entry,
@@ -126,6 +139,7 @@ namespace ScrumProj.Controllers
                 {
                     var thisFileId = entry.fileId;
                     var FileToFetch = db.Files.SingleOrDefault(i => i.FileId == thisFileId);
+
                     model.ListOfInformalEntriesToLoopInBlogView.Add(new EntryViewModel
                     {
                         entry = entry,
@@ -134,42 +148,53 @@ namespace ScrumProj.Controllers
 
                 }
             }
+
             model.ListOfComments = new List<Comment>();
             foreach (var c in db.Comments)
             {
                 model.ListOfComments.Add(c);
             }
+
             model.Categories = new List<Categories>();
             foreach (var c in db.Categories)
             {
                 model.Categories.Add(c);
             }
+
             model.CategoryIds = new List<CategoryInEntry>();
             foreach (var i in db.CategoryInEntrys)
             {
                 model.CategoryIds.Add(i);
             }
+
             return View(model);
         }
 
+
+
+        // Method
         public Models.File SaveFileToDatabase(HttpPostedFileBase newFile)
         {
-                AppDbContext db = new AppDbContext();
-                byte[] file;
-                string fileName;
-                using (var br = new BinaryReader(newFile.InputStream))
-                {
-                    file = br.ReadBytes((int)newFile.ContentLength);
-                    fileName = Path.GetFileName(newFile.FileName);
-                }
-                
-                return (new Models.File
-                {
-                    FileBytes = file,
-                    FileName = fileName
-                });
+            AppDbContext db = new AppDbContext();
+            byte[] file;
+            string fileName;
+
+            using (var br = new BinaryReader(newFile.InputStream))
+            {
+                file = br.ReadBytes((int)newFile.ContentLength);
+                fileName = Path.GetFileName(newFile.FileName);
+            }
             
+            return (new Models.File
+            {
+                FileBytes = file,
+                FileName = fileName
+            });                
         }
+
+
+
+        // Method
         public FileResult DownLoadFile(int id)
         {
             AppDbContext db = new AppDbContext();
@@ -178,34 +203,46 @@ namespace ScrumProj.Controllers
 
             byte[] fileBytes = FileById.FileBytes;
             string fileName = FileById.FileName;
-            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
-
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);   
         }
+
+
+
+        // Method
         public ActionResult DeleteEntry(int postId) {
             var ctx = new AppDbContext();
             var entry = ctx.Entries.Find(postId);
             var fileID = entry.fileId;
             var file = ctx.Files.Find(fileID);
+
             if (file != null)
             {
                 ctx.Files.Remove(file);
             }
             ctx.Entries.Remove(entry);
             ctx.SaveChanges();
+
             return RedirectToAction("BlogPage");
         }
+
+
+
+        // Method
         public ActionResult EditEntryView(EntryViewModel model, int postId, string Title, string Content, int? fileId)
         {
             var ctx = new AppDbContext();
             var ThisFile = new Models.File();
+
             if (model == null)
             {
                 model = new EntryViewModel();
             }
+
             if (fileId != null && fileId != 0)
             {
                 var OldFile = ctx.Files.Find(fileId);
                 var CurrentEntry = ctx.Entries.Find(postId);
+
                 model.entry = new Entry();
                 model.entry.Title = Title;
                 model.entry.Content = Content;
@@ -215,6 +252,7 @@ namespace ScrumProj.Controllers
             else
             {
                 var CurrentEntry = ctx.Entries.Find(postId);
+
                 model.entry = new Entry();
                 model.entry.Title = Title;
                 model.entry.Content = Content;
@@ -226,29 +264,37 @@ namespace ScrumProj.Controllers
             {
                 model.Categories.Add(c);
             }
+
             model.CategoryIds = new List<CategoryInEntry>();
             foreach (var i in ctx.CategoryInEntrys)
             {
                 model.CategoryIds.Add(i);
             }
+
             return View(model);
-            
         }
 
+
+
+        // Method
         public ActionResult EditEntry(HttpPostedFileBase newFile, EntryViewModel model, string searchInput)
         {
             var ctx = new AppDbContext();
+
             if (newFile != null)
             {
                 var ThisFile = new Models.File();
                 ThisFile = SaveFileToDatabase(newFile);
                 ctx.Files.Add(ThisFile);
                 int FileIdToUse = 1000000;
+
                 ctx.SaveChanges();
+
                 foreach (var f in ctx.Files)
                 {
                     FileIdToUse = f.FileId;
                 }
+
                 var post = ctx.Entries.Find(model.entry.Id);
                 post.AuthorId = User.Identity.GetUserId();
                 post.Content = model.entry.Content;
@@ -278,12 +324,17 @@ namespace ScrumProj.Controllers
 
             return RedirectToAction("BlogPage");
         }
+
+
+
+        // Method
         public ActionResult PostComment(EntryViewModel model, int postId)
         {
             var ctx = new AppDbContext();
             var currentUserId = User.Identity.GetUserId();
             var currentUser = GetCurrentUser(currentUserId);
             var comment = model.comment;
+
             ctx.Comments.Add(new Comment
             {
                 comment = comment,
@@ -291,9 +342,13 @@ namespace ScrumProj.Controllers
                 Author = GetNameOfLoggedInUser()
             });
             ctx.SaveChanges();
+
             return RedirectToAction("BlogPage");
         }
 
+
+
+        // Method
         public string GetNameOfLoggedInUser()
         {
             var ctx = new AppDbContext();
@@ -305,43 +360,64 @@ namespace ScrumProj.Controllers
 
             return FirstName + " " + LastName; 
         }
+
+
+
+        // Method
         public ActionResult RemoveFile(int postId)
         {
             var ctx = new AppDbContext();
             var post = ctx.Entries.Find(postId);
             var i = post.fileId;
             var file = ctx.Files.Find(post.fileId);
+
             ctx.Files.Remove(file);
             post.fileId = 0;
             ctx.SaveChanges();
+
             return RedirectToAction("BlogPage");
         }
+
+
+
+        // Method
         public JsonResult GetSearchValue(string search)
         {
             var ctx = new AppDbContext();
             List<string> allsearch = ctx.Categories.Where(x => x.Name.Contains(search)).Select(x => x.Name).ToList();
+
             return new JsonResult { Data = allsearch, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
+
+
+
+        // Method
         public ActionResult TestView()
         {
             var ctx = new AppDbContext();
             var model = ctx.Categories.ToList();
+
             return View(model);
         }
 
+
+
+        // Method
         public void AddCategoryToDatabase(string searchInput, int latestPost)
         {
             var ctx = new AppDbContext();
-
             // splits categories from the input with space and puts them in array
             Regex rgx = new Regex("[^a-zA-Z ]");
+
             var fixedString = rgx.Replace(searchInput, "");
             var strArray = fixedString.ToLower().Split(' ');
+
             foreach (var str in strArray)
             {
                 if (str != "")
                 {
                     string strWithHash = "#" + str;
+
                     //Checks if ínputdata matches with categories in the database.
                     if (ctx.Categories.Any(x => x.Name == str))
                     {
@@ -351,6 +427,7 @@ namespace ScrumProj.Controllers
                             CategoryId = ctx.Categories.Where(s => s.Name == str).Select(i => i.Id).Single()
                         });
                     }
+
                     //Checks if ínputdata matches with categories in the database if we add the # in the input.
                     else if (ctx.Categories.Any(x => x.Name == strWithHash))
                     {
@@ -360,6 +437,7 @@ namespace ScrumProj.Controllers
                             CategoryId = ctx.Categories.Where(s => s.Name == strWithHash).Select(i => i.Id).Single()
                         });
                     }
+
                     //If the input doesn't exist in the database we add them.
                     else if (!ctx.Categories.Any(x => x.Name == str))
                     {
@@ -378,11 +456,13 @@ namespace ScrumProj.Controllers
                             });
                         }
                         ctx.SaveChanges();
+
                         int CatId = 100000000;
                         foreach (var f in ctx.Categories)
                         {
                             CatId = f.Id;
                         }
+
                         ctx.CategoryInEntrys.Add(new CategoryInEntry
                         {
                             EntryId = latestPost,
@@ -394,14 +474,21 @@ namespace ScrumProj.Controllers
                 ctx.SaveChanges();
             }
         }
+
+
+
+        // Method
         public ActionResult DeleteCategory(int catId, int postId)
         {
             var ctx = new AppDbContext();
             var categoryToDelete = ctx.CategoryInEntrys.Find(catId);
+
             ctx.CategoryInEntrys.Remove(categoryToDelete);
             ctx.SaveChanges();
+
             var entry = ctx.Entries.Find(postId);
             var model = new EntryViewModel();
+
             return RedirectToAction("EditEntryView", new
             {
                 model,
@@ -411,6 +498,10 @@ namespace ScrumProj.Controllers
                 entry.fileId
             });
         }
+
+
+
+        // Method
         public ActionResult _DeleteCategoryPartial(EntryViewModel model)
         {
             return PartialView(model);
